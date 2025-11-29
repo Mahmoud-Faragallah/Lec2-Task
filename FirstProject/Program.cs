21a8f52c0d3c2ff199d552a25fd9aa2821f1c899
Mahmoud-Faragallah <mahmoud0faragallah@gmail.com>
2025-11-29 22:49:45 +0200

    Delete FirstProject directory

--- FirstProject/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿
-
-Console.WriteLine("Please Enter your favorite number between 1 and 100 ");
-int FNumber  = Convert.ToInt32(Console.ReadLine()) ;
-
-Console.WriteLine("No Really," + FNumber + " is my favorite number!");
